f10a25e27c3f332440f4c17f24d5093a6fe6777b
tbeek <tbeek6640@student.landstede.nl>
2018-11-15 15:50:35 +0100

    WIP

---
+++ app/model/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim11
+ * Date: 15-11-2018
+ * Time: 15:48
+ */
+
+class Home extends Model
+{
+    public function __construct()
+    {
+        parent::__construct("home");
+    }
+
+}
